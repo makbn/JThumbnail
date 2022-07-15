e6003c631f6ea7841ccaee6ee3ae9c9fd49e30d6
makbn <mehdi74akbarian@gmail.com>
2022-07-15 02:46:11 -0600

    code reformat

--- src/main/java/io/github/makbn/thumbnailer/Test.java
+++ src/main/java/io/github/makbn/thumbnailer/Test.java
@@ -2,25 +2,28 @@ package io.github.makbn.thumbnailer;
 
 import io.github.makbn.thumbnailer.listener.ThumbnailListener;
 import io.github.makbn.thumbnailer.model.ThumbnailCandidate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by Mehdi Akbarian-Rastaghi on 9/30/18
+ * @author Mehdi Akbarian Rastaghi on 9/30/18
  */
 
 public class Test {
+
+    private final static Logger mLog = LogManager.getLogger(Test.class);
 
     public static void main(String[] args) {
 
         try {
             AppSettings.init(args);
             Thumbnailer.start();
-            File in = new File("/Users/mehdiakbarian-rastaghi/Documents/Projects/Sakku/gharardad.docx");
+            var in = new File("PATH_TO_A_DOCUMENT");
             if (in.exists()) {
-                ThumbnailCandidate candidate = new ThumbnailCandidate(in, "unique_code");
-
+                var candidate = new ThumbnailCandidate(in, "unique_code");
                 Thumbnailer.createThumbnail(candidate, new ThumbnailListener() {
                     @Override
                     public void onThumbnailReady(String hash, File thumbnail) {
@@ -29,12 +32,12 @@ public class Test {
 
                     @Override
                     public void onThumbnailFailed(String hash, String message, int code) {
-                        System.out.println(message);
+                        System.err.printf("Message:%s\tCode:%d%n", message, code);
                     }
                 });
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            mLog.error(e);
         }
 
     }
