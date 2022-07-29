05a7cf0d3547e9f774166bdf62d9da6094f094d7
Mehdi Akbarian Rastaghi <mehdi74akbarian@gmail.com>
2022-07-28 20:45:13 -0600

    code cleanup

--- src/main/java/io/github/makbn/thumbnailer/AppSettings.java
+++ src/main/java/io/github/makbn/thumbnailer/AppSettings.java
@@ -3,23 +3,21 @@ package io.github.makbn.thumbnailer;
 import org.apache.commons.cli.*;
 
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 /**
  * created by Mehdi Akbarian-Rastaghi 2018-10-21
  */
 public class AppSettings {
+    public static final long FORCE_RECHECK_QUEUE_MS = 0L;
     private static final ResourceBundle rb = ResourceBundle.getBundle("application");
-
-    public static int DRIVE_OPENOFFICE_PORT;
-    public static String DRIVE_OPENOFFICE_SERVER_PATH;
-
+    public static int[] OPENOFFICE_PORTS;
+    public static String OPENOFFICE_PATH;
     public static String TEMP_DIR;
-    public static String DRIVE_DIR;
-
     public static int THUMB_WIDTH;
     public static int THUMB_HEIGHT;
     private static boolean init = false;
-
+    public static boolean HANDLE_RESTART = false;
 
     public static void init(String[] args) {
         CommandLineParser parser = new DefaultParser();
@@ -27,11 +25,10 @@ public class AppSettings {
         try {
             CommandLine cmd = parser.parse(options, args);
 
-            DRIVE_OPENOFFICE_PORT = Integer.parseInt(getValue(cmd, "openoffice_port"));
-            DRIVE_OPENOFFICE_SERVER_PATH = getValue(cmd, "openoffice_dir");
+            OPENOFFICE_PORTS = Stream.of(getValue(cmd, "openoffice_port").split(",")).mapToInt(Integer::valueOf).toArray();
+            OPENOFFICE_PATH = getValue(cmd, "openoffice_dir");
 
             TEMP_DIR = getValue(cmd, "temp_dir");
-            DRIVE_DIR = getValue(cmd, "drive_dir");
             THUMB_HEIGHT = Integer.parseInt(getValue(cmd, "thumb_height"));
             THUMB_WIDTH = Integer.parseInt(getValue(cmd, "thumb_width"));
             init = true;
@@ -45,7 +42,6 @@ public class AppSettings {
         options.addOption("p", "openoffice_port", true, "tcp port for openoffice/libreoffice.");
         options.addOption("od", "openoffice_dir", true, "open office home dir.");
         options.addOption("td", "temp_dir", true, "temp directory for saving thumb files.");
-        options.addOption("dd", "drive_dir", true, "drive directory to save thumb files.");
         options.addOption("h", "thumb_height", true, " thumbnail height size in px.");
         options.addOption("w", "thumb_width", true, "thumbnail width size in px.");
         return options;
@@ -59,7 +55,7 @@ public class AppSettings {
         }
     }
 
-    public static boolean isInit() {
+    public static boolean initialized() {
         return init;
     }
 }
