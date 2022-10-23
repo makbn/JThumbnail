d111a695f97b3dfd62ea9b51363ba91924deca87
makbn <mehdi74akbarian@gmail.com>
2022-10-22 18:40:48 -0600

    code cleanup exception handling improvement

--- src/main/java/io/github/makbn/thumbnailer/util/mime/XlsFileIdentifier.java
+++ src/main/java/io/github/makbn/thumbnailer/util/mime/XlsFileIdentifier.java
@@ -26,15 +26,16 @@
 
 package io.github.makbn.thumbnailer.util.mime;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
+@Log4j2
 public class XlsFileIdentifier extends OfficeFileIdentifier {
-    private static final Logger mLog = LogManager.getLogger("XlsFileIdentifier");
 
     public XlsFileIdentifier() {
         super();
@@ -45,15 +46,12 @@ public class XlsFileIdentifier extends OfficeFileIdentifier {
     public String identify(String mimeType, byte[] bytes, File file) {
 
         if (isOfficeFile(mimeType) && !XLS_MIME_TYPE.equals(mimeType)) {
-            try {
-                FileInputStream stream = new FileInputStream(file);
-                HSSFWorkbook workbook = new HSSFWorkbook(stream);
-
+            try(FileInputStream stream = new FileInputStream(file); HSSFWorkbook workbook = new HSSFWorkbook(stream)) {
                 if (workbook.getNumberOfSheets() != 0) {
                     return XLS_MIME_TYPE;
                 }
-            } catch (Throwable e) {
-                mLog.info(e);
+            }  catch (IOException | OfficeXmlFileException e) {
+                log.debug(e);
             }
         }
 
