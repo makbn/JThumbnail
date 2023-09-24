b6f60a32d4b59acf48c6ee013da125e23717ce2a
makbn <mehdi74akbarian@gmail.com>
2023-09-23 20:00:23 -0600

    renaming the package

--- src/main/java/io/github/makbn/jthumbnail/core/util/ResizeImage.java
+++ src/main/java/io/github/makbn/jthumbnail/core/util/ResizeImage.java
@@ -1,28 +1,7 @@
-/*
- * regain/Thumbnailer - A file search engine providing plenty of formats (Plugin)
- * Copyright (C) 2011  Come_IN Computerclubs (University of Siegen)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Contact: Come_IN-Team <come_in-team@listserv.uni-siegen.de>
- */
-
-package io.github.makbn.thumbnailer.util;
+package io.github.makbn.jthumbnail.core.util;
 
 
-import io.github.makbn.thumbnailer.exception.UnsupportedInputFileFormatException;
+import io.github.makbn.jthumbnail.core.exception.UnsupportedInputFileFormatException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
