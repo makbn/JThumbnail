948a42272ed5df030a570bae703b34bc452cc2d3
makbn <mehdi74akbarian@gmail.com>
2023-09-23 19:59:34 -0600

    adding springdoc to document the controller with open api v3

---
+++ src/main/java/io/github/makbn/jthumbnail/api/OpenAPIDocumentConfiguration.java
@@ -0,0 +1,23 @@
+package io.github.makbn.jthumbnail.api;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIDocumentConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${jthumbnailer.name}") String title, @Value("${jthumbnailer.desc}") String desc) {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .summary(desc)
+                        .title(title)
+                        .license(new License().name("GPL-2.0")
+                                .url("https://github.com/makbn/JThumbnail/blob/master/LICENSE")));
+    }
+}
---
+++ src/main/java/io/github/makbn/jthumbnail/core/config/OfficeManagerConfiguration.java
@@ -0,0 +1,51 @@
+package io.github.makbn.jthumbnail.core.config;
+
+import io.github.makbn.jthumbnail.core.exception.ThumbnailerRuntimeException;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.jodconverter.core.office.OfficeException;
+import org.jodconverter.core.office.OfficeManager;
+import org.jodconverter.local.office.ExistingProcessAction;
+import org.jodconverter.local.office.LocalOfficeManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration("JTOfficeManagerConfiguration")
+@DependsOn("JTApplicationConfig")
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class OfficeManagerConfiguration {
+    OfficeManager officeManager;
+
+    @Autowired
+    public OfficeManagerConfiguration(AppSettings settings) throws IOException {
+        File dir = settings.getOfficeDirectory();
+
+        this.officeManager = LocalOfficeManager.builder()
+                .portNumbers(settings.getOpenOfficePorts())
+                .workingDir(dir)
+                .processTimeout(settings.getTimeout())
+                .taskExecutionTimeout(settings.getTimeout())
+                .maxTasksPerProcess(settings.getMaxTaskPerProcess())
+                .existingProcessAction(ExistingProcessAction.CONNECT_OR_KILL)
+                .disableOpengl(true)
+                .officeHome(settings.getOpenOfficePath())
+                .install()
+                .build();
+        try {
+            officeManager.start();
+        } catch (OfficeException e) {
+            throw new ThumbnailerRuntimeException(e);
+        }
+    }
+
+    @Bean("office_manager")
+    public OfficeManager getOfficeManager() {
+        return officeManager;
+    }
+
+}
