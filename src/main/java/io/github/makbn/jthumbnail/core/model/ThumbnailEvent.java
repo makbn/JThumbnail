7a7b164201ccb62bd3f587ddad297cef1c6adbf1
makbn <mehdi74akbarian@gmail.com>
2023-09-23 20:02:45 -0600

    adding event vo object for spring monolith even

---
+++ src/main/java/io/github/makbn/jthumbnail/core/model/ThumbnailEvent.java
@@ -0,0 +1,24 @@
+package io.github.makbn.jthumbnail.core.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+import java.io.File;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class ThumbnailEvent {
+
+    public enum Status {
+        GENERATED, WAITING, FAILED
+    }
+
+    String uid;
+    Status status;
+    File thumbnailFile;
+}
